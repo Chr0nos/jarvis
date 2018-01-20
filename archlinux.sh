e1e4a05683f8e9ed3519e6c391a8d64b39fdba48
Chr0nos <snicolet@student.42.fr>
2018-01-20 21:49:01 +0000

    ajout de paquets a installer et changement de methode d'intallation

--- archlinux.sh
+++ archlinux.sh
@@ -3,12 +3,13 @@ NAME=archlinux
 VIDEO=extra/nvidia-dkms extra/nvidia-settings
 #VIDEO=extra/xf86-video-intel
 DEVELOPPER=python python-pip git clang make tig geany
-PASSWD=
 
-pacstrap $TARGET base base-devel xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet mate mate-extra
+pacstrap $TARGET base base-devel grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet mate mate-extra ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw
 echo $NAME >> $TARGET/etc/hostname
 arch-chroot $TARGET systemctl enable NetworkManager
 arch-chroot $TARGET systemctl enable lightdm
 arch-chroot $TARGET systemctl enable gpm
-arch-chroot $TARGET passwd $PASSWD
 arch-chroot $TARGET locale-gen
+arch-chroot $TARGET grub-mkconfig -o /boot/grub/grub.cfg
+arch-chroot $TARGET mkinitcpio -p linux -g /boot/initramfs-linux.img
+arch-chroot $TARGET passwd
