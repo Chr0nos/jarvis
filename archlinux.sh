bd50eccfe46c897ee76b169703edfb2b2c3d3d5c
Chr0nos <snicolet@student.42.fr>
2018-01-28 22:51:07 +0000

    ajout de choses tel que le choix de desktop environement

--- archlinux.sh
+++ archlinux.sh
@@ -1,14 +1,22 @@
 TARGET=/mnt
 NAME=archlinux
+#DESKTOP=extra/plasma-meta
+DESKTOP=mate mate-extra
+#DESKTOP=xfce4
 VIDEO=extra/nvidia-dkms extra/nvidia-settings
 #VIDEO=extra/xf86-video-intel
 DEVELOPPER=python python-pip git clang make tig geany
+EXTRA=
 
-pacstrap $TARGET base base-devel grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet mate mate-extra ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw
+pacstrap $TARGET base base-devel grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw fail2ban pavucontrol gparted ntfs-3g exfat-utils xorg-xrandr xorg-xinit sshfs ffmpegthumbnailer $EXTRA
 echo $NAME >> $TARGET/etc/hostname
+sed -i "s\# Defaults targetpw\Defaults targetpw\g" $TARGET/etc/sudoers
+sed -i "s\# %wheel ALL=(ALL) ALL\%wheel ALL=(ALL) ALL" $TARGET/etc/sudoers
 arch-chroot $TARGET systemctl enable NetworkManager
 arch-chroot $TARGET systemctl enable lightdm
 arch-chroot $TARGET systemctl enable gpm
+arch-chroot $TARGET systemctl enable fail2ban
+arch-chroot $TARGET systemctl enable smartd
 arch-chroot $TARGET locale-gen
 arch-chroot $TARGET grub-mkconfig -o /boot/grub/grub.cfg
 arch-chroot $TARGET mkinitcpio -p linux -g /boot/initramfs-linux.img
