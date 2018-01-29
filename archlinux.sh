43b6601265254ce017a05685864fb4071d876d3c
Chr0nos <snicolet@student.42.fr>
2018-01-29 03:36:59 +0000

    added some confirmations

--- archlinux.sh
+++ archlinux.sh
@@ -1,13 +1,19 @@
-TARGET=/mnt
-NAME=archlinux
-#DESKTOP=extra/plasma-meta
-DESKTOP=mate mate-extra
-#DESKTOP=xfce4
-VIDEO=extra/nvidia-dkms extra/nvidia-settings
-#VIDEO=extra/xf86-video-intel
-DEVELOPPER=python python-pip git clang make tig geany
+TARGET="/home/adamaru/jours/jarvis/test/"
+#TARGET="/mnt"
+NAME="archlinux"
+#DESKTOP="extra/plasma-meta"
+DESKTOP="mate mate-extra"
+#DESKTOP="xfce4"
+VIDEO="extra/nvidia-dkms extra/nvidia-settings"
+#VIDEO="extra/xf86-video-intel"
+DEVELOPPER="python python-pip git clang make tig geany"
 EXTRA=
 
+echo "I will install arch linux on ${TARGET}"
+echo "hostname: ${NAME}"
+echo "desktop environement: ${DESKTOP}"
+echo "extra: ${EXTRA}"
+read -p "Press enter to continue or ctrl+c to abort."
 pacstrap $TARGET base base-devel grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw fail2ban pavucontrol gparted ntfs-3g exfat-utils xorg-xrandr xorg-xinit sshfs ffmpegthumbnailer $EXTRA
 echo $NAME >> $TARGET/etc/hostname
 sed -i "s\# Defaults targetpw\Defaults targetpw\g" $TARGET/etc/sudoers
