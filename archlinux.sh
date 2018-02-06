0be21c537804335164959a3ceb10c75d7172b3a4
Chr0nos <snicolet@student.42.fr>
2018-02-06 19:48:28 +0000

    ajout de intel-ucode en dependence

--- archlinux.sh
+++ archlinux.sh
@@ -14,7 +14,7 @@ echo "hostname: ${NAME}"
 echo "desktop environement: ${DESKTOP}"
 echo "extra: ${EXTRA}"
 read -p "Press enter to continue or ctrl+c to abort."
-pacstrap $TARGET base base-devel grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw fail2ban pavucontrol gparted ntfs-3g exfat-utils xorg-xrandr xorg-xinit sshfs ffmpegthumbnailer $EXTRA
+pacstrap $TARGET base base-devel intel-ucode grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter libreoffice libreoffice-fr mpv vlc chromium gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw fail2ban pavucontrol gparted ntfs-3g exfat-utils xorg-xrandr xorg-xinit sshfs ffmpegthumbnailer $EXTRA
 echo $NAME >> $TARGET/etc/hostname
 sed -i "s\# Defaults targetpw\Defaults targetpw\g" $TARGET/etc/sudoers
 sed -i "s\# %wheel ALL=(ALL) ALL\%wheel ALL=(ALL) ALL" $TARGET/etc/sudoers
