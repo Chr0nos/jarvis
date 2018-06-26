4c767dd2a047a2b52355a29134aeb9f3aab94d9f
Sébastien Nicolet <snicolet@student.42.fr>
2018-06-26 16:01:32 +0200

    added install for trizen and the begin of the partitioning script

--- archlinux.sh
+++ archlinux.sh
@@ -1,5 +1,6 @@
 TARGET="/mnt"
 NAME="archlinux"
+DEVICE=/dev/sda
 
 #DESKTOP="extra/plasma-meta"
 DESKTOP="mate mate-extra"
@@ -30,6 +31,26 @@ echo "browser: ${BROWSER}"
 echo "extra: ${EXTRA}"
 read -p "Press enter to continue or ctrl+c to abort."
 
+# this function does the folowing partitions sheme:
+# mount point	size		flags
+# /boot			100M		boot
+# /				30Gb
+# swap			4Gb
+# /home			all remaining space of the drive
+function create_partitions() {
+	parted --script ${DEVICE} \
+		mklabel dos \
+		mkpart primary 1MiB 100MiB \
+		mkpart primary 100MiB 4100Mib \
+		mkpart primary 4100MiB 34GiB \
+		mkpart primary 34GiB \
+		set 1 boot on
+	mkfs.ext4 -L Boot ${DEVICE}1
+	mkfs.ext4 -L Arch ${DEVICE}2
+	mkswap ${DEVICE}3
+	mkfs.ext4 -L Home ${DEVICE}4
+}
+
 pacstrap $TARGET base base-devel $FIRMWARE grub xorg-server $VIDEO $DEVELOPPER networkmanager htop vim net-tools pulseaudio lightdm lightdm-gtk-greeter mpv gpm zsh terminator fish openssh openssl networkmanager-openvpn network-manager-applet ttf-liberation ttf-ubuntu-font-family xorg-fonts-75dpi xorg-fonts-100dpi ttf-dejavu ttf-freefont otf-font-awesome gnome-keyring smartmontools hdparm idle3-tools iw fail2ban pavucontrol gparted ntfs-3g exfat-utils xorg-xrandr xorg-xinit sshfs ffmpegthumbnailer $BROWSER $EXTRA
 # check for installation success
 if [ $? == 0 ]; then
@@ -45,6 +66,8 @@ if [ $? == 0 ]; then
 	arch-chroot $TARGET grub-mkconfig -o /boot/grub/grub.cfg
 	arch-chroot $TARGET mkinitcpio -p linux -g /boot/initramfs-linux.img
 	arch-chroot $TARGET passwd
+	# note this needs base-devel and git
+	arch-chroot $TARGET git clone https://aur.archlinux.org/trizen.git /tmp/trizen && cd /tmp/trizen && makepkg -si
 else
 	echo "failed to install base packages."
 fi
