b4bac786ea5efc546fb56bb3fe990c09c0a5027b
snicolet <snicolet@student.42.fr>
2018-08-08 17:04:11 +0200

    added permissions check

--- archclean.py
+++ archclean.py
@@ -17,7 +17,7 @@ def hsize(size):
     return ("{}{}".format(round(size, 2), units[i]))
 
 
-def package_clean(name, lst, pretend=True):
+def package_clean(name, lst, real=False):
     l = len(lst)
     if (l == 1):
         return(0)
@@ -31,13 +31,13 @@ def package_clean(name, lst, pretend=True):
             keep_file = False
             continue
         print("[RM  ] {:50} [{:4}]".format(filepath, hsize(filesize)))
-        if pretend == False:
+        if real == True:
             os.unlink(CACHE_DIR + filepath)
         gained_space += filesize
     return(gained_space)
 
 
-def packages_list(pretend=True):
+def packages_list(real=False):
     dico = {}
     base_name = re.compile(r'^([a-z-\d]+)')
     for file in os.listdir(CACHE_DIR):
@@ -49,8 +49,8 @@ def packages_list(pretend=True):
 
     gained_space = 0
     for key, pkg in dico.items():
-        gained_space += package_clean(key, pkg, pretend)
-    print("----\ngained space: {}".format(hsize(gained_space)))
+        gained_space += package_clean(key, pkg, real)
+    print("----\nGained space: {}".format(hsize(gained_space)))
 
 
 if __name__ == "__main__":
@@ -58,7 +58,11 @@ if __name__ == "__main__":
         print("usage: {} [-r/-p]".format(sys.argv[0]))
         print("if '-r' is not present, then the script will run in pretend mode");
         sys.exit(0)
-    pretend = sys.argv[1] == "-r"
-    packages_list(True);
-    if not pretend:
+    real = sys.argv[1] == "-r"
+    try:
+        packages_list(real);
+    except PermissionError:
+        print("Cannot delete files in {}, please run me as root.".format(
+            CACHE_DIR))
+    if not real:
         print("This script has run in pretend mode")
