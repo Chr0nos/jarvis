a95ebedc432fa010e7c55a23f28ce37a6b3d2e85
Chr0nos <snicolet@student.42.fr>
2018-12-08 23:46:44 +0100

    moved mydu to a dedicated dir, i don't like programs in one big file so it's now an independent project with a Makefile

---
+++ scripts/mydu/srcs/mydu.h
@@ -0,0 +1,64 @@
+#ifndef MYDU_H
+# define MYDU_H
+# include <string.h>
+# include <limits.h>
+# include "libft.h"
+
+# define FLAG_FULLPATH_DISPLAY	(1u << 0)
+# define FLAG_LOCALSTAT			(1u << 1)
+# define FLAG_REVERSE			(1u << 2)
+# define FLAG_EMPTY_NODES		(1u << 3)
+# define FLAG_ASCSV				(1u << 4)
+# define FILENAME_MAX			256
+
+struct config {
+	const char		*root;
+	size_t			flags;
+	size_t			path_len_align;
+	size_t			maxlen;
+	size_t			maxlevel;
+	int				(*sorter)(struct s_list *, struct s_list *);
+};
+
+struct nodestat {
+	size_t			local;
+	size_t			total;
+};
+
+struct node {
+	struct node     *parent;
+	struct s_list   *childs;
+	char            path[PATH_MAX];
+	char			name[FILENAME_MAX];
+	size_t			path_len;
+	struct nodestat	space;
+	struct nodestat	files;
+};
+
+#pragma pack(push, 4)
+
+struct parser_entry {
+	const int		letter;
+	const char		*name;
+	const size_t	flags;
+	const size_t	mask;
+
+};
+
+#pragma pack(pop)
+
+#define PARSER_ENTRIES 5
+
+static const struct parser_entry g_parsing_table[PARSER_ENTRIES] = {
+	(struct parser_entry){'p', "full-path", FLAG_FULLPATH_DISPLAY, 0},
+	(struct parser_entry){'r', "reverse", FLAG_REVERSE, 0},
+	(struct parser_entry){'l', "local", FLAG_LOCALSTAT, 0},
+	(struct parser_entry){'e', "empty", FLAG_EMPTY_NODES, 0},
+	(struct parser_entry){'c', "csv", FLAG_ASCSV, FLAG_LOCALSTAT}
+};
+
+int		parser(int ac, char **av, struct config *cfg);
+int     lst_cmp(t_list *a, t_list *b);
+int		lst_revcmp(t_list *a, t_list *b);
+
+#endif
---
+++ scripts/mydu/srcs/parser.c
@@ -0,0 +1,53 @@
+#include "mydu.h"
+
+static int		parser_loop(const char *input, struct config *cfg)
+{
+	const struct parser_entry		*ent;
+	size_t							p;
+
+	p = 0;
+	while (p < PARSER_ENTRIES)
+	{
+		ent = &g_parsing_table[p];
+		if (ent->letter == (int)input[1])
+		{
+			cfg->flags |= ent->flags;
+			cfg->flags &= ~ent->mask;
+			return (EXIT_SUCCESS);
+		}
+		p++;
+	}
+	return (EXIT_FAILURE);
+}
+
+int		parser(int ac, char **av, struct config *cfg)
+{
+	int		idx;
+
+	ft_bzero(cfg, sizeof(*cfg));
+	if (ac < 2)
+	{
+		ft_printf("usage: %s <path>\n", av[0]);
+		return (EXIT_FAILURE);
+	}
+	cfg->path_len_align = 42;
+	cfg->maxlen = 170;
+	cfg->maxlevel = (size_t)-1;
+	cfg->sorter = &lst_cmp;
+	for (idx = 1; idx < ac; idx++)
+	{
+		if (av[idx][0] != '-')
+			cfg->root = av[idx];
+		else if (parser_loop(av[idx], cfg) == EXIT_SUCCESS)
+			;
+		else if (ft_sscanf(av[idx], "--max-level=%lu", &cfg->maxlevel) == 1)
+			;
+		else
+			cfg->root = av[idx];
+	}
+	if (cfg->flags & FLAG_REVERSE)
+		cfg->sorter = &lst_revcmp;
+	if (!cfg->root)
+		return (EXIT_FAILURE);
+	return (EXIT_SUCCESS);
+}
