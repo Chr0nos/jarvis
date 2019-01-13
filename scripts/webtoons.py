a77e631447ff559063b5bf4eb4c1de08cf27e9ab
Chr0nos <snicolet@student.42.fr>
2019-01-13 18:27:50 +0100

    changed user agent version

--- scripts/webtoons.py
+++ scripts/webtoons.py
@@ -184,7 +184,7 @@ class WebToons():
 		i = 0
 		header = {
 			'Referer': 'www.webtoons.com',
-			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36'
+			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3112.113 Safari/537.36'
 		}
 		for url in lst:
 			filepath = "{}/{:04}.jpg".format(target, i)
