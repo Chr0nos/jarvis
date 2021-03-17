01979f8365f967a5390f5ff5edd1b577cae8d2ef
Chr0nos <snicolet@student.42.fr>
2021-03-17 12:42:49 +0100

    removed archinstall, it now has a v2 and it's own repo

--- scripts/archinstall/arch/exceptions.py
+++
@@ -1,6 +0,0 @@
-class CommandFail(Exception):
-    pass
-
-
-class ConfigError(Exception):
-    pass
--- scripts/archinstall/test.py
+++
@@ -1,9 +0,0 @@
-from arch.archinstall import FileFromHost, File
-
-if __name__ == "__main__":
-    custom_servers = [
-        'http://192.168.2.3:8080'
-    ]
-
-    mirrors = FileFromHost('/dev/shm/mirrorlist', '/')
-    mirrors.insert(File.to_config(custom_servers, prepend='Server '), line_index=3)
