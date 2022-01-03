baf7e8441c02da443ff4f8547b0dfa83ae1682f2
Sebastien Nicolet <snicolet95@gmail.com>
2022-01-03 11:44:00 +0100

    removed useless provider

--- scripts/webtoons/providers/__init__.py
+++ scripts/webtoons/providers/__init__.py
@@ -1,6 +1,5 @@
 from .webtoons import WebToon
 from .lelscan import LelScan
-from .nhentai import NHentaiToon
 from .mangaorigin import MangaOriginToon
 from .toomics import Toomic
 from .scantrad import ScanTrad
